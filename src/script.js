9898403ba5b5965812926f2e312feacc9c9942aa
rupertforbes <127436772+rupertforbes@users.noreply.github.com>
2023-10-10 12:33:53 +0100

    Added JS to display Tokyo time and date, imported title font and made CSS adjustments

--- src/script.js
+++ src/script.js
@@ -20,3 +20,18 @@ function newTimeZone(event) {
 let select = document.querySelector("#select-city");
 select.addEventListener("change", newTimeZone);
 //                                                "It is Saturday, October 8, 2023 2:51 PM in Europe/Paris"
+
+//tokyo time and date
+
+let tokyoDate = moment().tz("Asia/Tokyo").format("MMMM Do, YYYY");
+let tokyoDateTarget = document.querySelector("#tokyo-date");
+tokyoDateTarget.innerHTML = tokyoDate;
+
+let tokyoTime = moment().tz("Asia/Tokyo").format("h:mm:ss");
+let tokyoAmPm = moment().tz("Asia/Tokyo").format("A");
+let tokyoTimeTarget = document.querySelector(".tokyo-time");
+tokyoTimeTarget.innerHTML = `${tokyoTime} <span class="tokyo-am-pm">${tokyoAmPm}</span>`;
+
+//paris time and date
+
+//new york time and date
