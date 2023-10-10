5ca0eff720a593ef344bb08c6c85716a5dc37e63
rupertforbes <127436772+rupertforbes@users.noreply.github.com>
2023-10-10 12:54:44 +0100

    Added JS to include Paris and NY time and date

--- src/script.js
+++ src/script.js
@@ -22,16 +22,44 @@ select.addEventListener("change", newTimeZone);
 //                                                "It is Saturday, October 8, 2023 2:51 PM in Europe/Paris"
 
 //tokyo time and date
+function tokyTimeAndDate() {
+  let tokyoDate = moment().tz("Asia/Tokyo").format("MMMM Do, YYYY");
+  let tokyoDateTarget = document.querySelector(".tokyo-date");
+  tokyoDateTarget.innerHTML = tokyoDate;
 
-let tokyoDate = moment().tz("Asia/Tokyo").format("MMMM Do, YYYY");
-let tokyoDateTarget = document.querySelector("#tokyo-date");
-tokyoDateTarget.innerHTML = tokyoDate;
+  let tokyoTime = moment().tz("Asia/Tokyo").format("h:mm:ss");
+  let tokyoAmPm = moment().tz("Asia/Tokyo").format("A");
+  let tokyoTimeTarget = document.querySelector(".tokyo-time");
+  tokyoTimeTarget.innerHTML = `${tokyoTime} <span class="am-pm">${tokyoAmPm}</span>`;
+}
 
-let tokyoTime = moment().tz("Asia/Tokyo").format("h:mm:ss");
-let tokyoAmPm = moment().tz("Asia/Tokyo").format("A");
-let tokyoTimeTarget = document.querySelector(".tokyo-time");
-tokyoTimeTarget.innerHTML = `${tokyoTime} <span class="tokyo-am-pm">${tokyoAmPm}</span>`;
+setInterval(tokyTimeAndDate, 1000);
 
 //paris time and date
+function parisTimeAndDate() {
+  let parisDate = moment().tz("Europe/Paris").format("MMMM Do, YYYY");
+  let parisDateTarget = document.querySelector(".paris-date");
+  parisDateTarget.innerHTML = parisDate;
+
+  let parisTime = moment().tz("Europe/Paris").format("h:mm:ss");
+  let parisAmPm = moment().tz("Europe/Paris").format("A");
+  let parisTimeTarget = document.querySelector(".paris-time");
+  parisTimeTarget.innerHTML = `${parisTime} <span class="am-pm">${parisAmPm}</span>`;
+}
+
+setInterval(parisTimeAndDate, 1000);
 
 //new york time and date
+
+function newYorkTimeAndDate() {
+  let newYorkDate = moment().tz("America/New_York").format("MMMM Do, YYYY");
+  let newYorkDateTarget = document.querySelector(".new-york-date");
+  newYorkDateTarget.innerHTML = newYorkDate;
+
+  let newYorkTime = moment().tz("America/New_York").format("h:mm:ss");
+  let newYorkAmPm = moment().tz("America/New_York").format("A");
+  let newYorkTimeTarget = document.querySelector(".new-york-time");
+  newYorkTimeTarget.innerHTML = `${newYorkTime} <span class="am-pm">${newYorkAmPm}</span>`;
+}
+
+setInterval(newYorkTimeAndDate, 1000);
